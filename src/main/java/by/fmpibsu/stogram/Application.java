c5f2e6fde22ef3dc7c68835a7103e4a75fa97e09
Marmotikon <msemenov93057@gmail.com>
2023-05-30 16:58:29 +0300

    add loging

--- src/main/java/by/fmpibsu/stogram/Application.java
+++ src/main/java/by/fmpibsu/stogram/Application.java
@@ -1,5 +1,7 @@
 package by.fmpibsu.stogram;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +9,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 @EnableRedisHttpSession
 @SpringBootApplication
+@Slf4j
 public class Application {
+
+//	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+//	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -18,6 +27,7 @@ public class Application {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
+			log.info("Entering application");
 			System.out.println("Server running!");
 		};
 	}
