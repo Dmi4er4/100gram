9ebc0c779a59a28921d5fa0fafd8bfadda222465
Дмитрий Че <39742182+Dmi4er4@users.noreply.github.com>
2023-05-30 17:05:31 +0300

    Dev (#3)
    * first commit
    
    * implement dao
    
    * improve dao
    
    * develop chat page
    
    * first commit
    
    * Create database connection, write sql scripts in dao, update entities
    
    * implement chat creation
    
    * connect user dao to db
    
    * implement static message loading
    
    * configure caching
    
    * implement dynamic message loading
    
    * implement registration
    
    * fix duplicate messages
    
    * add loging
    
    * delete loginController
    
    ---------
    
    Co-authored-by: toharrius <antonkharasau@gmail.com>
    Co-authored-by: Marmotikon <msemenov93057@gmail.com>

--- src/main/java/by/fmpibsu/stogram/Application.java
+++ src/main/java/by/fmpibsu/stogram/Application.java
@@ -7,10 +7,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import lombok.extern.slf4j.Slf4j;
+
 @EnableRedisHttpSession
 @SpringBootApplication
+@Slf4j
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -18,6 +20,7 @@ public class Application {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
+			log.info("Entering application");
 			System.out.println("Server running!");
 		};
 	}
