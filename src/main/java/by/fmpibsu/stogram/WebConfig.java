6301abf90534a40fbbbd372684f7debdf688c421
Дмитрий Че <39742182+Dmi4er4@users.noreply.github.com>
2023-05-31 13:22:35 +0300

    Dev to master (#4)
    * first commit
    
    * implement dao
    
    * improve dao
    
    * develop chat page
    
    * first commit
    
    * Create database connection, write sql scripts in dao, update entities
    
    * implement chat creation
    
    * connect user dao to db
    
    * implement static message loading
    
    * configure caching
    
    * implement dynamic message loading
    
    * implement registration
    
    * fix duplicate messages
    
    * add loging
    
    * delete loginController
    
    * add logs into Auth and Chat controllers, add unit tests for chat service
    
    * improve entities
    
    * improve entities
    
    ---------
    
    Co-authored-by: toharrius <antonkharasau@gmail.com>
    Co-authored-by: Marmotikon <msemenov93057@gmail.com>

--- src/main/java/by/fmpibsu/stogram/WebConfig.java
+++ src/main/java/by/fmpibsu/stogram/WebConfig.java
@@ -1,5 +1,6 @@
 package by.fmpibsu.stogram;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,10 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("Add Resource Handlers");
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("/static/")
                 .addResourceLocations("/static/js")
